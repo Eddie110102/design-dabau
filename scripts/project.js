fd7b38ecfbba53de55b07fd31c22c46c04ab80b4
Eddie110102 <eddiewang@ispan.com.tw>
2025-03-29 21:16:51 +0800

    [add] project page

---
+++ scripts/project.js
@@ -0,0 +1,341 @@
+const projectList = [
+  {
+    name: "台北市敦化南路-酒吧",
+    folder: "tpe-bar-store",
+    image: [
+      "tpe-bar-1.jpg",
+      "tpe-bar-2.jpg",
+      "tpe-bar-3.jpg",
+      "tpe-bar-4.jpg",
+      "tpe-bar-5.jpg",
+    ],
+  },
+  {
+    name: "新北市板橋龍泉街-住家",
+    folder: "ntp-longquan-house",
+    image: [
+      "ntp-longquan-1.jpg",
+      "ntp-longquan-2.jpg",
+      "ntp-longquan-3.jpg",
+      "ntp-longquan-4.jpg",
+      "ntp-longquan-5.jpg",
+      "ntp-longquan-6.jpg",
+      "ntp-longquan-7.jpg",
+      "ntp-longquan-8.jpg",
+      "ntp-longquan-9.jpg",
+      "ntp-longquan-10.jpg",
+      "ntp-longquan-11.jpg",
+      "ntp-longquan-12.jpg",
+      "ntp-longquan-13.jpg",
+      "ntp-longquan-14.jpg",
+      "ntp-longquan-15.jpg",
+      "ntp-longquan-16.jpg",
+      "ntp-longquan-17.jpg",
+    ],
+  },
+  {
+    name: "新北市新莊幸福東路-廠辦",
+    folder: "ntp-xingfueast-office",
+    image: [
+      "ntp-xingfueast-1.jpg",
+      "ntp-xingfueast-2.jpg",
+      "ntp-xingfueast-3.jpg",
+      "ntp-xingfueast-4.jpg",
+      "ntp-xingfueast-5.jpg",
+    ],
+  },
+  {
+    name: "新北市新莊幸福路-住家",
+    folder: "ntp-xingfu-house",
+    image: [
+      "ntp-xingfu-1.jpg",
+      "ntp-xingfu-2.jpg",
+      "ntp-xingfu-3.jpg",
+      "ntp-xingfu-4.jpg",
+      "ntp-xingfu-5.jpg",
+      "ntp-xingfu-6.jpg",
+      "ntp-xingfu-7.jpg",
+      "ntp-xingfu-8.jpg",
+      "ntp-xingfu-9.jpg",
+    ],
+  },
+  {
+    name: "新北市樹林三寶街-住家",
+    folder: "ntp-sanbao-house",
+    image: [
+      "ntp-sanbao-1.jpg",
+      "ntp-sanbao-2.jpg",
+      "ntp-sanbao-3.jpg",
+      "ntp-sanbao-4.jpg",
+      "ntp-sanbao-5.jpg",
+      "ntp-sanbao-6.jpg",
+      "ntp-sanbao-7.jpg",
+      "ntp-sanbao-8.jpg",
+      "ntp-sanbao-9.jpg",
+      "ntp-sanbao-10.jpg",
+      "ntp-sanbao-11.jpg",
+    ],
+  },
+  {
+    name: "新北市五股新五路三段-木工裝潢",
+    folder: "ntp-xin5th-carpenter",
+    image: [
+      "ntp-xin5th-carpenter-1.jpg",
+      "ntp-xin5th-carpenter-2.jpg",
+      "ntp-xin5th-carpenter-3.jpg",
+      "ntp-xin5th-carpenter-4.jpg",
+      "ntp-xin5th-carpenter-5.jpg",
+      "ntp-xin5th-carpenter-6.jpg",
+      "ntp-xin5th-carpenter-7.jpg",
+      "ntp-xin5th-carpenter-8.jpg",
+      "ntp-xin5th-carpenter-9.jpg",
+      "ntp-xin5th-carpenter-10.jpg",
+      "ntp-xin5th-carpenter-11.jpg",
+      "ntp-xin5th-carpenter-12.jpg",
+      "ntp-xin5th-carpenter-13.jpg",
+      "ntp-xin5th-carpenter-14.jpg",
+      "ntp-xin5th-carpenter-15.jpg",
+    ],
+  },
+  {
+    name: "新北市板橋懷德街-住家",
+    folder: "ntp-huaide-house",
+    image: [
+      "ntp-huaide-1.jpg",
+      "ntp-huaide-2.jpg",
+      "ntp-huaide-3.jpg",
+      "ntp-huaide-4.jpg",
+      "ntp-huaide-5.jpg",
+      "ntp-huaide-6.jpg",
+      "ntp-huaide-7.jpg",
+      "ntp-huaide-8.jpg",
+      "ntp-huaide-9.jpg",
+    ],
+  },
+  {
+    name: "台北市信義寶徠花園-住家",
+    folder: "tpe-polarisgarden-house",
+    image: [
+      "tpe-polarisgarden-1.jpg",
+      "tpe-polarisgarden-2.jpg",
+      "tpe-polarisgarden-3.jpg",
+      "tpe-polarisgarden-4.jpg",
+      "tpe-polarisgarden-5.jpg",
+      "tpe-polarisgarden-6.jpg",
+      "tpe-polarisgarden-7.jpg",
+      "tpe-polarisgarden-8.jpg",
+      "tpe-polarisgarden-9.jpg",
+      "tpe-polarisgarden-10.jpg",
+      "tpe-polarisgarden-11.jpg",
+    ],
+  },
+  {
+    name: "桃園大連三街-住家",
+    folder: "tyn-dalian-house",
+    image: [
+      "tyn-dalian-1.jpg",
+      "tyn-dalian-2.jpg",
+      "tyn-dalian-3.jpg",
+      "tyn-dalian-4.jpg",
+    ],
+  },
+  {
+    name: "桃園南平路-住家",
+    folder: "tyn-nanping-house",
+    image: [
+      "tyn-nanping-1.jpg",
+      "tyn-nanping-2.jpg",
+      "tyn-nanping-3.jpg",
+      "tyn-nanping-4.jpg",
+      "tyn-nanping-5.jpg",
+      "tyn-nanping-6.jpg",
+      "tyn-nanping-7.jpg",
+      "tyn-nanping-8.jpg",
+      "tyn-nanping-9.jpg",
+      "tyn-nanping-10.jpg",
+      "tyn-nanping-11.jpg",
+      "tyn-nanping-12.jpg",
+    ],
+  },
+  {
+    name: "新竹縣竹北-住家",
+    folder: "hsh-zhubei-house",
+    image: [
+      "hsh-zhubei-1.jpg",
+      "hsh-zhubei-2.jpg",
+      "hsh-zhubei-3.jpg",
+      "hsh-zhubei-4.jpg",
+      "hsh-zhubei-5.jpg",
+      "hsh-zhubei-6.jpg",
+      "hsh-zhubei-7.jpg",
+      "hsh-zhubei-8.jpg",
+      "hsh-zhubei-9.jpg",
+      "hsh-zhubei-10.jpg",
+      "hsh-zhubei-11.jpg",
+      "hsh-zhubei-12.jpg",
+      "hsh-zhubei-13.jpg",
+      "hsh-zhubei-14.jpg",
+      "hsh-zhubei-15.jpg",
+      "hsh-zhubei-16.jpg",
+      "hsh-zhubei-17.jpg",
+      "hsh-zhubei-18.jpg",
+      "hsh-zhubei-19.jpg",
+      "hsh-zhubei-20.jpg",
+      "hsh-zhubei-21.jpg",
+      "hsh-zhubei-22.jpg",
+      "hsh-zhubei-23.jpg",
+      "hsh-zhubei-24.jpg",
+      "hsh-zhubei-25.jpg",
+      "hsh-zhubei-26.jpg",
+      "hsh-zhubei-27.jpg",
+      "hsh-zhubei-28.jpg",
+      "hsh-zhubei-29.jpg",
+      "hsh-zhubei-30.jpg",
+      "hsh-zhubei-31.jpg",
+      "hsh-zhubei-32.jpg",
+    ],
+  },
+  {
+    name: "新北市板橋光環路-油漆工程",
+    folder: "ntp-guanghuan-paint",
+    image: [
+      "ntp-guanghuan-1.jpg",
+      "ntp-guanghuan-2.jpg",
+      "ntp-guanghuan-3.jpg",
+      "ntp-guanghuan-4.jpg",
+      "ntp-guanghuan-5.jpg",
+      "ntp-guanghuan-6.jpg",
+      "ntp-guanghuan-7.jpg",
+      "ntp-guanghuan-8.jpg",
+      "ntp-guanghuan-9.jpg",
+      "ntp-guanghuan-10.jpg",
+    ],
+  },
+  {
+    name: "新北市永和得和路-住家",
+    folder: "ntp-dehe-house",
+    image: [
+      "ntp-dehe-1.jpg",
+      "ntp-dehe-2.jpg",
+      "ntp-dehe-3.jpg",
+      "ntp-dehe-4.jpg",
+      "ntp-dehe-5.jpg",
+      "ntp-dehe-6.jpg",
+      "ntp-dehe-7.jpg",
+      "ntp-dehe-8.jpg",
+      "ntp-dehe-9.jpg",
+      "ntp-dehe-10.jpg",
+      "ntp-dehe-11.jpg",
+      "ntp-dehe-12.jpg",
+    ],
+  },
+  {
+    name: "台北市內湖潭美街-室內設計",
+    folder: "tpe-tanmei-interior",
+    image: [
+      "tpe-tanmei-1.jpg",
+      "tpe-tanmei-2.jpg",
+      "tpe-tanmei-3.jpg",
+      "tpe-tanmei-4.jpg",
+      "tpe-tanmei-5.jpg",
+      "tpe-tanmei-6.jpg",
+      "tpe-tanmei-7.jpg",
+      "tpe-tanmei-8.jpg",
+      "tpe-tanmei-9.jpg",
+      "tpe-tanmei-10.jpg",
+      "tpe-tanmei-11.jpg",
+      "tpe-tanmei-12.jpg",
+      "tpe-tanmei-13.jpg",
+      "tpe-tanmei-14.jpg",
+      "tpe-tanmei-15.jpg",
+      "tpe-tanmei-16.jpg",
+      "tpe-tanmei-17.jpg",
+    ],
+  },
+  {
+    name: "台北市中和民有街-木工裝潢",
+    folder: "ntp-minyou-carpenter",
+    image: [
+      "ntp-minyou-1.jpg",
+      "ntp-minyou-2.jpg",
+      "ntp-minyou-3.jpg",
+      "ntp-minyou-4.jpg",
+      "ntp-minyou-5.jpg",
+      "ntp-minyou-7.jpg",
+      "ntp-minyou-8.jpg",
+      "ntp-minyou-9.jpg",
+      "ntp-minyou-10.jpg",
+      "ntp-minyou-11.jpg",
+      "ntp-minyou-12.jpg",
+      "ntp-minyou-13.jpg",
+      "ntp-minyou-14.jpg",
+      "ntp-minyou-15.jpg",
+      "ntp-minyou-16.jpg",
+      "ntp-minyou-17.jpg",
+      "ntp-minyou-18.jpg",
+      "ntp-minyou-19.jpg",
+      "ntp-minyou-20.jpg",
+      "ntp-minyou-21.jpg",
+      "ntp-minyou-22.jpg",
+      "ntp-minyou-23.jpg",
+      "ntp-minyou-24.jpg",
+      "ntp-minyou-25.jpg",
+      "ntp-minyou-26.jpg",
+      "ntp-minyou-27.jpg",
+      "ntp-minyou-28.jpg",
+    ],
+  },
+];
+
+const projectNav = document.querySelector("#projectNav");
+const title = document.querySelector("#projectLightGallery h2");
+
+for (let i = 0; i < projectList.length; i++) {
+  const div = document.createElement("div");
+  div.setAttribute("class", "projectTag");
+  div.textContent = projectList[i].name;
+  div.addEventListener("click", function () {
+    title.textContent = projectList[i].name;
+    loadImages(i);
+  });
+  projectNav.appendChild(div);
+}
+
+// gallary
+
+const gallary = document.querySelector("#static-thumbnails");
+loadImages(0);
+
+function loadImages(tag) {
+  title.textContent = projectList[tag].name;
+  gallary.innerHTML = "";
+  for (let j = 0; j < projectList[tag].image.length; j++) {
+    // const box = document.createElement("div");
+    // box.setAttribute("class", "projectBox");
+    const a = document.createElement("a");
+    a.setAttribute(
+      "href",
+      `./images/project/${projectList[tag].folder}/${projectList[tag].image[j]}`
+    );
+    const image = document.createElement("img");
+    image.setAttribute(
+      "src",
+      `./images/project/${projectList[tag].folder}/${projectList[tag].image[j]}`
+    );
+    // image.setAttribute("alt", projectList[tag].image[j]);
+
+    a.appendChild(image);
+    // box.appendChild(a);
+    gallary.appendChild(a);
+
+    lightGallery(document.getElementById("static-thumbnails"), {
+      thumbnail: true,
+      mode: "lg-fade",
+      closeOnTap: true,
+      download: false,
+      enableSwipe: true,
+      escKey: true,
+      thumbHeight: "0px",
+    });
+  }
+}
